31e69eedddca8cb7c17bb0c98f5985b3fddef8ea
Noah Sano <noahcsano@berkeley.edu>
2023-07-10 15:37:15 -0700

    upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Noah Sano'"; //letter subject
+	$to = 'noahcsano@gmail.com'; // Enter Your E-mail
+	$from = 'noahcsano@gmail.com'; // Admin e-mail
+	$fromName = 'Noah Sano'; // Your company name
+	$charset = 'UTF-8';
+?>
